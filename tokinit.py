ccd67c361c1ddcc4cad341e274683619e6e28474
TianlongWang <tianlong.wang.13@googlemail.com>
2012-04-10 00:50:39 -0400

    new url for sample

--- tokinit.py
+++ tokinit.py
@@ -3,7 +3,7 @@ import OpenTokSDK
 def get_session_token():
 	api_key = '13652392' # Replace with your OpenTok API key.
 	api_secret = 'ca0d3ebf52449ad8e3a89497eea96fe18c496076'  # Replace with your OpenTok API secret.
-	session_address = 'presencerobot.appspot.com' # Replace with the representative URL of your session.
+	session_address = 'empty-light-6387.herokuapp.com' # Replace with the representative URL of your session.
 	opentok_sdk = OpenTokSDK.OpenTokSDK(api_key, api_secret, staging=True)
 	session_properties = {OpenTokSDK.SessionProperties.p2p_preference: "enabled"}
 	session = opentok_sdk.create_session(session_address, session_properties)
